8d3fa5d0d5d98e9c969a8aa6298aa507ce5972c1
abdurrahman2004 <ar978874@gmail.com>
2025-03-21 22:31:17 +0530

    Backend basics initialised

--- Spotify-Backend/server.js
+++ Spotify-Backend/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config'
 import songRouter from "./src/routes/songRouter.js";
+import connectDB from "./src/config/Mongodb.js";
+import connecCloudinary from "./src/config/Cloudinary.js";
 
 //app config
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+connecCloudinary();
 
 //middlewares
 app.use(express.json()); // the request are parsed using json method
--- Spotify-Backend/src/routes/songRouter.js
+++ Spotify-Backend/src/routes/songRouter.js
@@ -1,9 +1,10 @@
 import { addSong , listSong } from "../controllers/SongController.js";
 import express from "express"
+import upload from "../middlewares/multer.js";
 
 const songRouter = express.Router();
 
-songRouter.post('/add',addSong);
-songRouter.get('/list',listSong);
+songRouter.post('/add',upload.fields([{name: 'image',maxCount:1},{name: 'audio',maxCount:1}]),addSong);
+songRouter.get('/list',upload,listSong);
 
 export default songRouter
