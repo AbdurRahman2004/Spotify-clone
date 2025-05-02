981917f5498d7463d20cdd7f86d25cecf52625f5
abdurrahman2004 <ar978874@gmail.com>
2025-05-03 00:03:13 +0530

    Issue with verscel fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-  build: {
-    outDir: 'dist',
-  },
+  plugins: [react()],
   server: {
-    port: 5173,
-  },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true
+      }
+    },
+    host: '0.0.0.0',
+    port: 5173
+  }
 })
