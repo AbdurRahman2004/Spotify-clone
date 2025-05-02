79e8d2452399ef2526912c94514f22c9121eb417
abdurrahman2004 <ar978874@gmail.com>
2025-05-02 23:59:54 +0530

    Issue with verscel fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+  },
 })
